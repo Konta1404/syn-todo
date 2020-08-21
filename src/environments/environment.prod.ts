8c2a1b55321cfb5ae00023960bf60b8881b6d0a8
Veselin <veselin.kontic@synergysuite.com>
2020-08-22 00:04:35 +0200

    Adds firebase config for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCd1Ajf21cT3HAaHHvFlHTtZ6nC1Ku4cJM",
+    authDomain: "syn-todo.firebaseapp.com",
+    databaseURL: "https://syn-todo.firebaseio.com",
+    projectId: "syn-todo",
+    storageBucket: "syn-todo.appspot.com",
+    messagingSenderId: "201071366512",
+    appId: "1:201071366512:web:13552843963c53d685bfb5",
+    measurementId: "G-MMESY8LQD0"
+  }
 };
